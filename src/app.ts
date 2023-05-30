17e4f475f67c7b56a7f46006e7450be1c38ac00f
Khaled Ayman - Personal <khaledayman-personal@Khaleds-MacBook-Pro.local>
2023-05-30 18:33:17 +0300

    Added Typescript configuration to the project

---
+++ src/app.ts
@@ -0,0 +1,27 @@
+import express, { Request, Response } from 'express';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+// Middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// Routes
+app.get('/', (req: Request, res: Response) => {
+  res.send('Hello, TypeScript!');
+});
+
+// Error handling middleware
+app.use((err: Error, req: Request, res: Response) => {
+  console.error(err.stack);
+  res.status(500).send('Something broke!');
+});
+
+// Start server
+app.listen(port, () => {
+  console.log(`Server started on port ${port}`);
+});
